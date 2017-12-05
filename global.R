bdc3bb83aad99b94e7cc0474caff6b3743ce7e90
TobiasRoth <t.roth@unibas.ch>
2017-12-05 12:09:41 +0100

    tgras and fgras needs re-projection to work proparly.

--- global.R
+++ global.R
@@ -5,10 +5,6 @@
 #------------------------------------------------------------------------------
 # Libraries ----
 #------------------------------------------------------------------------------
-# packages <- c("shiny", "leaflet", "shinydashboard", "raster", "RColorBrewer", "spdep", "vegan")
-# if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-#   install.packages(setdiff(packages, rownames(installed.packages())))  
-# }
 library(shiny)
 library(leaflet)
 library(shinydashboard)
@@ -34,16 +30,20 @@ colanzeige <- "light-blue"
 # Daten laden und vorbereiten ----
 #------------------------------------------------------------------------------
 # Daten Modelle laden
-load("Modelle/raTotTG.RData")
 load("Modelle/raTotFG.RData")
+load("Modelle/raTotTG.RData")
 
 # Daten Bestehende Infrastruktur laden
 load("BestehendeInfrastruktur/fgras.RData")
-load("BestehendeInfrastruktur/tgras.RData")
-load("BestehendeInfrastruktur/fg.RData")
-load("BestehendeInfrastruktur/tg.RData")
+fgras <- projectRasterForLeaflet(fgras)
 load("BestehendeInfrastruktur/fgvern.RData")
+load("BestehendeInfrastruktur/fg.RData")
+
+load("BestehendeInfrastruktur/tgras.RData")
+tgras <- projectRasterForLeaflet(tgras)
+load("BestehendeInfrastruktur/tg.RData")
 load("BestehendeInfrastruktur/tgvern.RData")
+
 load("Hintergrunddaten/perimeter.RData")
 
 #------------------------------------------------------------------------------
@@ -65,19 +65,3 @@ Dazu wird die bestehende Infrastruktur mit der geringstmöglichen Distanz mitein
 wird das Verbindungsstück rot eingezeichnet. Für diese Verbindung wären zusätzliche
 Trittsteine wünschenswert."
 
-#------------------------------------------------------------------------------
-# Vernetzung berechnen ----
-#------------------------------------------------------------------------------
-# map <- leaflet("mymap")
-# ref <- "http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-# map <- addTiles(map, urlTemplate = ref)
-# vernb <- spTransform(vern, CRS("+init=epsg:21781"))
-# vern1 <- buffer(vernb, 500)
-# vern1 <- spTransform(vern1, CRS("+init=epsg:4326"))
-# map <- addPolygons(map, data = vern1, stroke = FALSE, opacity = 10, fillColor = "yellow")
-# map <- addPolylines(map, data = vern[SpatialLinesLengths(vern, longlat = TRUE) <= 10], color = "blue")
-# map <- addPolylines(map, data = vern[SpatialLinesLengths(vern, longlat = TRUE) > 10], color = "red")
-# map
-# 
-# SpatialLinesLengths(vern, longlat = TRUE)
-# SpatialLinesLengths(spTransform(vern, CRS("+init=epsg:21781")), longlat = TRUE)
